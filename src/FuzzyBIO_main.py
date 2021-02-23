113907a3b54fad537e217fd0741361dffdb53e8a
AnneDirkson <a.r.dirkson@liacs.leidenuniv.nl>
2021-02-23 10:04:54 +0100

    Updated main script to make NER optional and removed outdated Concept normalization

--- src/FuzzyBIO_main.py
+++ src/FuzzyBIO_main.py
@@ -6,11 +6,6 @@
 # - turn the BIOHD annotations into FuzzyBIO annotations
 # - train NER models (10-fold CV) using distilbert (cased)
 # - analyse the results of the NER models trained using FuzzyBIO and using BIOHD
-# 
-# optional: 
-# - Link the extracted entities to the concept normalization annotations provided with perfect entities 
-# - Train concept normalization models (10-fold CV) using distilbert (uncased)
-# - Analyse the results of these models on perfect data, imperfect BIOHD and imperfect FuzzyBIO data 
 
 # In[6]:
 
@@ -50,7 +45,7 @@ def save_obj(obj, name):
 # In[8]:
 
 
-def main(data, folddict, outfolder, ConceptNorm = False, txt_dict = None, concept_dict = None, modelpath = None): 
+def main(data, folddict, outfolder, runNER = False): 
     ##making the fuzzybio annotations
     data.columns = ['sent_id', 'word', 'tag']
     data_agg = data.groupby('sent_id').agg(list)
@@ -62,42 +57,33 @@ def main(data, folddict, outfolder, ConceptNorm = False, txt_dict = None, concep
     
     os.mkdir(outfolder + "/NER/FuzzyBIO/") 
     os.mkdir(outfolder + "/NER/BIOHD/") 
-    ##NER for Fuzzy data
-    BERTforNER().main(outfolder + "/NER/FuzzyBIO/", nwdata, folddict)
     
-    ##NER for BIOHD data
-    BERTforNER().main(outfolder + "/NER/BIOHD/", data, folddict)
-
-    ##NER Results -- all evaluation is done on the BIOHD tagged data
-    predfolder1 = outfolder + '/NER/BIOHD/'
-    predfolder2 = outfolder + "/NER/FuzzyBIO/"
-    
-    NER_results().main(data, folddict, predfolder1, predfolder2)   
-    
-    if ConceptNorm == True:
-        f, f2, f3 = AnnotateImperfect().main(data, nwdata, folddict, concept_dict, txt_dict, predfolder1, predfolder2, tagtype = 'ADR')
-        ##f3 are the perfect annotations
-        os.mkdir(outfolder + "/ConceptNorm/") 
+    if runNER == True:
+        ##NER for Fuzzy data
+        BERTforNER().main(outfolder + "/NER/FuzzyBIO/", nwdata, folddict)
         
-        ConceptNorm().main(f3, preddata1= f, preddata2=f2, outpath = outfolder + '/ConceptNorm/')
+        ##NER for BIOHD data
+        BERTforNER().main(outfolder + "/NER/BIOHD/", data, folddict)
+    
+        ##NER Results -- all evaluation is done on the BIOHD tagged data
+        predfolder1 = outfolder + '/NER/BIOHD/'
+        predfolder2 = outfolder + "/NER/FuzzyBIO/"
         
-        ConceptNormEval().main(modelpath, predpath= outfolder + '/ConceptNorm/', orig_data =f3, basic_extr=f, simplify_extr=f2)
+        NER_results().main(data, folddict, predfolder1, predfolder2)   
+        
 
 
 # In[ ]:
 
 
 ## Example usage with CADEC 
+## Comment if you want to import the class as a module 
 
 data = load_obj('./data/CADEC_BIOHD')
 folddict = load_obj('./data/folddict_CADEC')
-txtdict = load_obj('./data/txtdict_CADEC')
-conceptdict = load_obj('./data/conceptdict_CADEC')
-
-modelpath = './data/mednorm_raw_10n_40l_5w_64dim.bin'
 
 os.mkdir('./Results/") 
 outfolder = './Results/'
 
-main(data, folddict, outfolder, ConceptNorm = True, txt_dict = txtdict, concept_dict = conceptdict, modelpath = modelpath): 
+main(data, folddict, outfolder, runNER = True, txt_dict = txtdict, concept_dict = conceptdict, modelpath = modelpath)
 
